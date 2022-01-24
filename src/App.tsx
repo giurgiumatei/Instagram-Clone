261faaab9c7178691de756b6fa9529557e502ad6
DESKTOP-3L3V23H\giurg <giurgiu.matei@yahoo.com>
2022-01-24 19:27:17 +0200

    finished instagram header

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+        <div className="app__header">
+            <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                 alt="Instagram"
+            />
+        </div>
     </div>
   );
 }
