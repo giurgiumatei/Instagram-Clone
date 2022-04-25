c97f152858fc0f7c790c69cdff98049c17cf6718
DESKTOP-3L3V23H\giurg <giurgiu.matei@yahoo.com>
2022-04-25 15:56:23 +0300

    began working on sign-up

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,42 @@ import Post from "./Post";
 import { db } from "./Firebase/firebase";
 import { getPosts } from "./Firebase/Queries/Posts/GetPosts";
 import { DocumentData } from "firebase/firestore/lite";
+import { Button, Modal } from "@mui/material";
+import { makeStyles } from '@mui/styles';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+  return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+const useStyles = makeStyles( (theme : any) => ({
+  modal: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  paper: {
+      position: 'absolute',
+      width: 450,
+      backgroundColor: theme.palette.background.paper,
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+const signUp = (event: any) => {
+
+}
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
   const [posts, setPosts] = useState<DocumentData[]>([]);
+  const [open, setOpen] = useState<boolean>(false);
 
   useEffect(() => {
     async function fetchPosts() {
@@ -19,20 +52,32 @@ function App() {
 
   return (
     <div className="App">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <h2>I am a modal</h2>
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
           alt="Instagram"
         />
       </div>
-      {posts.map(post => {
+
+      <Button onClick={() => setOpen(true)}>
+        Sign Up
+      </Button>
+
+      {posts.map((post) => {
         return (
           <Post
             {...console.log(post)}
             key={post.id}
             username={post.data.username}
             caption={post.data.caption}
-            imageUrl={post.data.imageUrl} />
+            imageUrl={post.data.imageUrl}
+          />
         );
       })}
     </div>
